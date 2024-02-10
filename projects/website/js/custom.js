e30a29dbc97f4d18eef32300e388022ffe5dd105
vibratewaves <vibratewaves@gmail.com>
2024-02-10 21:55:27 +0530

    DN Ready to launch

---
+++ projects/website/js/custom.js
@@ -0,0 +1,18 @@
+// let slideIndex = 1;
+// const slides = document.getElementsByClassName("slide");
+// const intervalTime = 5000;
+
+// function showSlide(n) {
+//   for (let i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex = n >= slides.length ? 0 : n < 0 ? slides.length - 1 : n;
+//   slides[slideIndex].style.display = "block";
+// }
+
+// setInterval(() => {
+//   showSlide(slideIndex + 1);
+// }, intervalTime);
+
+// setTimeout(showSlide(1), 500);
+// setTimeout(showSlide(0), 5000);
